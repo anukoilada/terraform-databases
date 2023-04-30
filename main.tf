d24892290a4841262c70faa833a4d1c0d0b4f6a0
anupama <anupama@gmail.com>
2023-04-30 16:46:14 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ module "docdb" {
   ENV                       = var.ENV
   DOCDB_PORT                = var.DOCDB_PORT
   DOCDB_INSTANCE_CLASS      = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
 }
