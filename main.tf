5842a2e78959308581aa80fcfdc04a79f6115f32
anupama <anupama@gmail.com>
2023-04-30 02:03:47 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 module "docdb" {
   source                    = "./vendor/modules/docdb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it 
   ENV                       = var.ENV
+  DOCDB_PORT                = var.DOCDB_PORT
+  DOCDB_INSTANCE_CLASS      = var.DOCDB_INSTANCE_CLASS
 }
