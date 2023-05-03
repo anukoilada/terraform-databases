e5d6795b286cbf635fc665172d2f5dfbcc809ba2
anupama <anupama@gmail.com>
2023-05-02 22:34:29 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -6,7 +6,3 @@ module "docdb" {
   DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
 }
 
-module "redis" {
-  source                       = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
-  ENV                          = var.ENV
-}
